f77d5badae64578f0e1cc1f4d4739e80b49c8cca
BokiGaja <bgajic93@gmail.com>
2019-02-08 20:53:27 +0100

    Task finished

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -94,6 +94,9 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="cars">Cars <span class="sr-only">(current)</span></a>
                     </li>
